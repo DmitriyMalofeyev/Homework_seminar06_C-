1bab56f43bafcf4116b350a896dcb17fd17603d1
DmitriyMalofeyev <et5@list.ru>
2022-07-16 21:04:23 +0300

    добавлено решение дз по семинару 6

---
+++ Homework_seminar06_C-/Task 41/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Укажите какое количество чисел вы собираетесь ввести "); 
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число " + (i + 1));
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Количество введенных чисел больше нуля: " + count); 
---
+++ Homework_seminar06_C-/Task 43/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем   */
+// находим чему равен x и y из формул:
+// x=(b1-b2)/(k2-k1)
+// y = k1 * x + b1
+
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+if (k2 != k1)
+{
+    x = (b1 - b2) / (k2 - k1);
+    y = k1 * x + b1;
+    Console.WriteLine("Координата х точки пересечения прямых: " + x);
+    Console.WriteLine("Координата y точки пересечения прямых: " + y);
+}
+else
+{
+    Console.WriteLine("k2 и k1 не могут быть равны");
+}
+ 
+
