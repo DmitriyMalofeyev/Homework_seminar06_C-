821817a974233f90981a9e02843d45bbdb68a20d
DmitriyMalofeyev <et5@list.ru>
2022-07-20 22:39:21 +0300

    переделана задача 41

--- Homework_seminar06_C-/Task 41/Program.cs
+++ Homework_seminar06_C-/Task 41/Program.cs
@@ -1,19 +1,18 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
 
-Console.WriteLine("Укажите какое количество чисел вы собираетесь ввести "); 
-int n = Convert.ToInt32(Console.ReadLine());
-
-int[] array = new int[n];
+Console.WriteLine("Введите через пробел любое количество чисел: "); 
+string? input = Console.ReadLine();
+int[] data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                  .Select(int.Parse)
+                  .ToArray();
 int count = 0;
 
-for (int i = 0; i < n; i++)
+foreach (var i in data)
 {
-    Console.WriteLine("Введите число " + (i + 1));
-    array[i] = Convert.ToInt32(Console.ReadLine());
-    if (array[i] > 0)
+    if (i > 0)
     {
         count++;
-    }
+    } 
 }
 
 Console.WriteLine("Количество введенных чисел больше нуля: " + count); 
